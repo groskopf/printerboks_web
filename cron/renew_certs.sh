cac6f43c7ea42e4d1f45ac9182bc6c3409af50b9
Paul Groskopf <paul@groskopf-embedded.com>
2022-11-26 20:36:59 +0000

    Fixed cron elseware

--- cron/renew_certs.sh
+++ cron/renew_certs.sh
@@ -2,6 +2,6 @@
 
 cd /workdir
 echo "Renewing Let's Encrypt Certificates... (`date`)"
-docker-compose run --name certbot_renew --entrypoint certbot certbot renew
+docker-compose run --entrypoint certbot certbot renew
 echo "Reloading Nginx configuration"
 docker-compose exec -T nginx nginx -s reload
