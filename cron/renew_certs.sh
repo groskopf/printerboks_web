77df4e4b751a58b2d59a4a1878aaad7e4b021d3b
Paul Groskopf <paul@groskopf-embedded.com>
2023-06-01 19:06:39 +0000

    Try to fix the cron problem with certificate renewal

--- cron/renew_certs.sh
+++ cron/renew_certs.sh
@@ -2,6 +2,6 @@
 
 cd /workdir
 echo "Renewing Let's Encrypt Certificates... (`date`)"
-docker-compose run --entrypoint certbot certbot renew
+docker compose run --rm --no-deps --no-TTY --entrypoint certbot certbot renew --no-random-sleep-on-renew
 echo "Reloading Nginx configuration"
 docker-compose exec -T nginx nginx -s reload
